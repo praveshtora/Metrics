f645fc837dc1d5737a18a4bf85d43865e70e28f1
AbhishekPalsule <abhishek.palsule@gmail.com>
2016-11-05 16:16:20 +0530

    domain objects

---
+++ src/main/java/com/example/dao/ThreePAdao.java
@@ -0,0 +1,8 @@
+package com.example.dao;
+
+import com.example.domain.ThreePA;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ThreePAdao extends PagingAndSortingRepository<ThreePA,Long>{
+
+}
---
+++ src/main/java/com/example/domain/DDX.java
@@ -0,0 +1,46 @@
+package com.example.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by palsulea on 11/5/2016.
+ */
+@Entity
+public class DDX {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private int headCount;
+    @Column
+    private int portofolios;
+    @Column
+    private int scoreCard;
+
+    public int getHeadCount() {
+        return headCount;
+    }
+
+    public void setHeadCount(int headCount) {
+        this.headCount = headCount;
+    }
+
+    public int getPortofolios() {
+        return portofolios;
+    }
+
+    public void setPortofolios(int portofolios) {
+        this.portofolios = portofolios;
+    }
+
+    public int getScoreCard() {
+        return scoreCard;
+    }
+
+    public void setScoreCard(int scoreCard) {
+        this.scoreCard = scoreCard;
+    }
+}
